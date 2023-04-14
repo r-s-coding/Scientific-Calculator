cf8c58ad625ed52bf58440b6e362bbf996c5b450
r-s-coding <112776326+r-s-coding@users.noreply.github.com>
2023-04-14 22:32:49 +0000

    Added the sliding animation and updated the styles

--- src/script.js
+++ src/script.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    const btnGroup = document.querySelector('.btn-toggle');
-    const toggleButtons = btnGroup.querySelectorAll('.btn');
-  
-    btnGroup.addEventListener('click', function() {
-      toggleButtons.forEach(button => {
-        button.classList.toggle('active');
-      });
+  const btnGroup = document.querySelector('.btn-toggle');
+  const toggleButtons = btnGroup.querySelectorAll('.btn');
+
+  btnGroup.addEventListener('click', function () {
+    toggleButtons.forEach(button => {
+      button.classList.toggle('active');
     });
+
+    // Sliding transition for the small screen
+    const carousel = document.querySelector('.carousel');
+    const slideIndex = document.querySelector('.btn-bd-toggleButton.active').textContent === '123' ? 0 : 1;
+    carousel.setAttribute('data-bs-slide-to', slideIndex);
+    carousel.dispatchEvent(new Event('slide.bs.carousel'));
+    const bsCarousel = new bootstrap.Carousel(carousel);
+    bsCarousel.to(slideIndex); // Trigger carousel slide transition
+  });
 });
